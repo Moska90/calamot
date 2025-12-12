12f65b678a46d3002d6e467a9c4b8958e78c1d48
Moska90 <hugomaso06@gmail.com>
2025-12-12 22:47:45 +0100

    Added dhparam

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -24,7 +24,68 @@ LGREY="\e[97m"
 BOLD="\e[1m"
 RESET="\e[0m"
 
+# Global variable to hold the PID of the spinner function
+SPINNER_PID=
+
+# Function to draw the spinner animation
+function start_spinner {
+    # Define the spinner frames
+    local spin='/-\|'
+    # Define the message to display
+    local message="$1"
+    
+    # 2>&1 means redirect stderr (2) to stdout (1)
+    # The while loop runs in the background
+    (
+        # Turn off case-insensitive matching for shopt (if it were on)
+        # Toggles behavior of shell options
+        trap "echo -e \"\n\n\"" SIGINT # Handle Ctrl+C gracefully
+        
+        while :
+        do
+            # Cycle through the spinner frames
+            for i in $(seq 0 3); do
+                # Get the current frame character
+                local char=${spin:i:1}
+                # Print the frame, overwrite the line using \r (carriage return)
+                printf "\r$message... %s" "$char"
+                sleep 0.1
+            done
+        done
+    ) &
+
+    # Store the PID of the background process so we can kill it later
+    SPINNER_PID=$!
+    # Disown the process so it doesn't get a SIGHUP when the script ends,
+    # though we intend to kill it explicitly.
+    disown
+}
+
+# Function to stop the spinner and clean up
+function stop_spinner {
+    # Check if the spinner process ID is set and running
+    if [[ -n "$SPINNER_PID" ]]; then
+        # Kill the background process
+        kill $SPINNER_PID 2>/dev/null
+        
+        # Clear the line and move the cursor to the beginning
+        printf "\r%40s\r" ""
+        SPINNER_PID=
+    fi
+}
+
+
 # Script
+function check_nginx() {
+    if dpkg -s nginx &>/dev/null then
+        echo " nginx instalado, saltando instalacion"
+    else
+        start spinner " Instalando nginx"
+        apt install nginx -y > /dev/null
+        stop_spinner
+}
+
+
 function newcomer() {
     clear
     echo -e "$LMAGENTA"
@@ -65,7 +126,56 @@ function vhost_https_server_config() {
     }
     server {
         listen 80;
-        listen [::]:80;
+        listen [::]:80;# Global variable to hold the PID of the spinner function
+SPINNER_PID=
+
+# Function to draw the spinner animation
+function start_spinner {
+    # Define the spinner frames
+    local spin='/-\|'
+    # Define the message to display
+    local message="$1"
+    
+    # 2>&1 means redirect stderr (2) to stdout (1)
+    # The while loop runs in the background
+    (
+        # Turn off case-insensitive matching for shopt (if it were on)
+        # Toggles behavior of shell options
+        trap "echo -e \"\n\n\"" SIGINT # Handle Ctrl+C gracefully
+        
+        while :
+        do
+            # Cycle through the spinner frames
+            for i in $(seq 0 3); do
+                # Get the current frame character
+                local char=${spin:i:1}
+                # Print the frame, overwrite the line using \r (carriage return)
+                printf "\r$message... %s" "$char"
+                sleep 0.1
+            done
+        done
+    ) &
+
+    # Store the PID of the background process so we can kill it later
+    SPINNER_PID=$!
+    # Disown the process so it doesn't get a SIGHUP when the script ends,
+    # though we intend to kill it explicitly.
+    disown
+}
+
+# Function to stop the spinner and clean up
+function stop_spinner {
+    # Check if the spinner process ID is set and running
+    if [[ -n "$SPINNER_PID" ]]; then
+        # Kill the background process
+        kill $SPINNER_PID 2>/dev/null
+        
+        # Clear the line and move the cursor to the beginning
+        printf "\r%40s\r" ""
+        SPINNER_PID=
+    fi
+}
+
 
         server_name $domain www.$domain;
 
@@ -104,6 +214,8 @@ function vhost_https_server_config() {
 }
 
 # Zona del script modular
+# Check if nginx is installed
+check_nginx
 # Checking for user domain
 newcomer
 # Check if domain key and crt exist
